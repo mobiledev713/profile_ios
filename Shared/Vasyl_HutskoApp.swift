54985ff0be6f609c1a8cc98b007f32c35ecd9119
Vasyl Hutsko <vasyl.hutsko713@outlook.com>
2021-07-18 22:36:18 -0400

    Initial Commit

---
+++ Shared/Vasyl_HutskoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vasyl_HutskoApp.swift
+//  Shared
+//
+//  Created by admin on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Vasyl_HutskoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
